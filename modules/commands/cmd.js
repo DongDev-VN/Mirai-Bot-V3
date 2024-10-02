7d797d00323e62239fcfb0f7e1076e3b6271fea3
Phạm Minh Đồng <dongdz.user@gmail.com>
2024-10-02 17:49:02 +0700

    update

--- modules/commands/cmd.js
+++ modules/commands/cmd.js
@@ -7,120 +7,79 @@ module.exports.config = {
     commandCategory: "Admin",
     usages: "[load/unload/loadAll/unloadAll/info] [tên module]",
     cooldowns: 5,
-    prefix: false,
-    dependencies: {
-        "fs-extra": "",
-        "child_process": "",
-        "path": ""
-    }
+    prefix: true
 };
 
 const loadCommand = function ({ moduleList, threadID, messageID }) {
-
-    const { execSync } = global.nodemodule['child_process'];
-    const { writeFileSync, unlinkSync, readFileSync } = global.nodemodule['fs-extra'];
-    const { join } = global.nodemodule['path'];
+    const { writeFileSync, readFileSync } = require('fs-extra');
+    const { join } = require('path');
     const { configPath, mainPath, api } = global.client;
     const logger = require(mainPath + '/utils/log');
-
     var errorList = [];
     delete require['resolve'][require['resolve'](configPath)];
     var configValue = require(configPath);
-    writeFileSync(configPath + '.temp', JSON.stringify(configValue, null, 2), 'utf8');
+
     for (const nameModule of moduleList) {
         try {
             const dirModule = __dirname + '/' + nameModule + '.js';
             delete require['cache'][require['resolve'](dirModule)];
             const command = require(dirModule);
             global.client.commands.delete(nameModule);
+
             if (!command.config || !command.run || !command.config.commandCategory) 
                 throw new Error('Module không đúng định dạng!');
-            global.client['eventRegistered'] = global.client['eventRegistered']['filter'](info => info != command.config.name);
-            if (command.config.dependencies && typeof command.config.dependencies == 'object') {
-                const listPackage = JSON.parse(readFileSync('./package.json')).dependencies,
-                    listbuiltinModules = require('module')['builtinModules'];
-                for (const packageName in command.config.dependencies) {
-                    var tryLoadCount = 0,
-                        loadSuccess = ![],
-                        error;
-                    const moduleDir = join(global.client.mainPath, 'nodemodules', 'node_modules', packageName);
-                    try {
-                        if (listPackage.hasOwnProperty(packageName) || listbuiltinModules.includes(packageName)) global.nodemodule[packageName] = require(packageName);
-                        else global.nodemodule[packageName] = require(moduleDir);
-                    } catch {
-                        logger.loader('Không tìm thấy package ' + packageName + ' hỗ trợ cho module ' + command.config.name+ 'tiến hành cài đặt...', 'warn');
-                        const insPack = {};
-                        insPack.stdio = 'inherit';
-                        insPack.env = process.env ;
-                        insPack.shell = !![];
-                        insPack.cwd = join(global.client.mainPath,'nodemodules')
-                        execSync('npm --package-lock false --save install ' + packageName + (command.config.dependencies[packageName] == '*' || command.config.dependencies[packageName] == '' ? '' : '@' + command.config.dependencies[packageName]), insPack);
-                        for (tryLoadCount = 1; tryLoadCount <= 3; tryLoadCount++) {
-                            require['cache'] = {};
-                            try {
-                                if (listPackage.hasOwnProperty(packageName) || listbuiltinModules.includes(packageName)) global.nodemodule[packageName] = require(packageName);
-                                else global.nodemodule[packageName] = require(moduleDir);
-                                loadSuccess = !![];
-                                break;
-                            } catch (erorr) {
-                                error = erorr;
-                            }
-                            if (loadSuccess || !error) break;
-                        }
-                        if (!loadSuccess || error) throw 'Không thể tải package ' + packageName + (' cho module ') + command.config.name +', lỗi: ' + error + ' ' + error['stack'];
+
+            global.client['eventRegistered'] = global.client['eventRegistered'].filter(info => info != command.config.name);
+            
+            if (command.config.envConfig && typeof command.config.envConfig === 'object') {
+                try {
+                    for (const [key, value] of Object.entries(command.config.envConfig)) {
+                        if (typeof global.configModule[command.config.name] === 'undefined') 
+                            global.configModule[command.config.name] = {};
+                        if (typeof configValue[command.config.name] === 'undefined') 
+                            configValue[command.config.name] = {};
+                        
+                        global.configModule[command.config.name][key] = configValue[command.config.name][key] || value || '';
+                        configValue[command.config.name][key] = configValue[command.config.name][key] || value || '';
                     }
+                    logger.loader('Loaded config ' + command.config.name);
+                } catch (error) {
+                    throw new Error('Không thể tải config module, lỗi: ' + JSON.stringify(error));
                 }
-                logger.loader('Đã tải thành công toàn bộ package cho module' + command.config.name);
             }
-            if (command.config.envConfig && typeof command.config.envConfig == 'Object') try {
-                for (const [key, value] of Object['entries'](command.config.envConfig)) {
-                    if (typeof global.configModule[command.config.name] == undefined) 
-                        global.configModule[command.config.name] = {};
-                    if (typeof configValue[command.config.name] == undefined) 
-                        configValue[command.config.name] = {};
-                    if (typeof configValue[command.config.name][key] !== undefined) 
-                        global.configModule[command.config.name][key] = configValue[command.config.name][key];
-                    else global.configModule[command.config.name][key] = value || '';
-                    if (typeof configValue[command.config.name][key] == undefined) 
-                        configValue[command.config.name][key] = value || '';
+
+            if (command['onLoad']) {
+                try {
+                    command['onLoad']({ configValue });
+                } catch (error) {
+                    throw new Error('Không thể onLoad module, lỗi: ' + JSON.stringify(error), 'error');
                 }
-                logger.loader('Loaded config' + ' ' + command.config.name);
-            } catch (error) {
-                throw new Error('Không thể tải config module, lỗi: ' + JSON.stringify(error));
             }
-            if (command['onLoad']) try {
-                const onLoads = {};
-                onLoads['configValue'] = configValue;
-                command['onLoad'](onLoads);
-            } catch (error) {
-                throw new Error('Không thể onLoad module, lỗi: ' + JSON.stringify(error), 'error');
-            }
+
             if (command.handleEvent) global.client.eventRegistered.push(command.config.name);
-            (global.config.commandDisabled.includes(nameModule + '.js') || configValue.commandDisabled.includes(nameModule + '.js')) 
-            && (configValue.commandDisabled.splice(configValue.commandDisabled.indexOf(nameModule + '.js'), 1),
-            global.config.commandDisabled.splice(global.config.commandDisabled.indexOf(nameModule + '.js'), 1))
-            global.client.commands.set(command.config.name, command)
+            if (global.config.commandDisabled.includes(nameModule + '.js') || configValue.commandDisabled.includes(nameModule + '.js')) {
+                configValue.commandDisabled.splice(configValue.commandDisabled.indexOf(nameModule + '.js'), 1);
+                global.config.commandDisabled.splice(global.config.commandDisabled.indexOf(nameModule + '.js'), 1);
+            }
+            global.client.commands.set(command.config.name, command);
             logger.loader('Loaded command ' + command.config.name + '!');
         } catch (error) {
-            errorList.push('- ' + nameModule + ' reason:' + error + ' at ' + error['stack']);
-        };
+            errorList.push('- ' + nameModule + ' reason: ' + error + ' at ' + error.stack);
+        }
     }
-    if (errorList.length != 0) api.sendMessage('Những module đã xảy ra sự cố khi đang load: ' + errorList.join(' '), threadID, messageID);
-    api.sendMessage('Loaded ' + (moduleList.length - errorList.length) + ' module(s)', threadID, messageID) 
-    writeFileSync(configPath, JSON.stringify(configValue, null, 4), 'utf8')
-    unlinkSync(configPath + '.temp');
-    return;
-}
 
+    if (errorList.length !== 0) {
+        api.sendMessage('Những module đã xảy ra sự cố khi đang load: ' + errorList.join(' '), threadID, messageID);
+    }
+    api.sendMessage('Loaded ' + (moduleList.length - errorList.length) + ' module(s)', threadID, messageID);
+    writeFileSync(configPath, JSON.stringify(configValue, null, 4), 'utf8');
+};
 const unloadModule = function ({ moduleList, threadID, messageID }) {
-    const { writeFileSync, unlinkSync } = global.nodemodule["fs-extra"];
+    const { writeFileSync, unlinkSync } = require("fs-extra");
     const { configPath, mainPath, api } = global.client;
     const logger = require(mainPath + "/utils/log").loader;
-
     delete require.cache[require.resolve(configPath)];
     var configValue = require(configPath);
-    writeFileSync(configPath + ".temp", JSON.stringify(configValue, null, 4), 'utf8');
-
     for (const nameModule of moduleList) {
         global.client.commands.delete(nameModule);
         global.client.eventRegistered = global.client.eventRegistered.filter(item => item !== nameModule);
@@ -128,20 +87,13 @@ const unloadModule = function ({ moduleList, threadID, messageID }) {
         global.config["commandDisabled"].push(`${nameModule}.js`);
         logger(`Unloaded command ${nameModule}!`);
     }
-
     writeFileSync(configPath, JSON.stringify(configValue, null, 4), 'utf8');
-    unlinkSync(configPath + ".temp");
-
     return api.sendMessage(`Unloaded ${moduleList.length} module(s)`, threadID, messageID);
 }
-
 module.exports.run = function ({ event, args, api }) {
-  //  if (event.senderID != 100004253741257) return api.sendMessage(`Quyền lồn biên giới!`, event.threadID, event.messageID)
-    const { readdirSync } = global.nodemodule["fs-extra"];
+    const { readdirSync } = require("fs-extra");
     const { threadID, messageID } = event;
-
     var moduleList = args.splice(1, args.length);
-
     switch (args[0]) {
         case "load": {
             if (moduleList.length == 0) return api.sendMessage("Tên module không được để trống!", threadID, messageID);
